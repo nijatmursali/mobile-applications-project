89a59174763f64e3c3e88a14c5eb4b224d30746c
Nijat Mursali <nijatmursali@gmail.com>
2020-12-02 15:09:29 +0100

    added mongoose and dotenv

--- api/index.js
+++ api/index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("database connected"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -10,7 +21,13 @@ app.get("/", (req, res) => {
 app.get("/api/questions", (req, res) => {
   let rdata = fs.readFileSync(path.resolve(__dirname, "questions.json"));
   let questions = JSON.parse(rdata);
-  res.send(questions);
+  res.json(questions);
+});
+
+app.get("/api/questions/:category", (req, res) => {
+  let rdata = fs.readFileSync(path.resolve(__dirname, "questions.json"));
+  let questions = JSON.parse(rdata);
+  res.send(questions[0]["category"]);
 });
 
 const port = process.env.PORT || 5000;
