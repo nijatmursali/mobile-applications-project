8b78488ab6ee42cd7613f72a70eaf6a90a0d5796
HassaanQureshi <HassaanAhmedQureshi@gmail.com>
2020-12-07 04:04:43 +0100

    final push
    - UI updated.
    - App working perfectly.

--- quizapp/app/src/main/java/com/sapienza/quizapp/MainActivity.kt
+++ quizapp/app/src/main/java/com/sapienza/quizapp/MainActivity.kt
@@ -5,11 +5,8 @@ import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -35,63 +32,100 @@ class MainActivity : AppCompatActivity() {
 
         val params = LinearLayout.LayoutParams(
             ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT)
+            ViewGroup.LayoutParams.MATCH_PARENT)
         params.setMargins(30,30,30,30)
 
-        val cardLinearLayout = LinearLayout(this)
-        cardLinearLayout.orientation = LinearLayout.VERTICAL
-//        cardLinearLayout.layoutParams = params
+        val linearLayout = LinearLayout(this)
+            linearLayout.orientation = LinearLayout.VERTICAL
 
 
+        //My Code
+        val myCardView = androidx.cardview.widget.CardView(this)
+            myCardView.layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                500)
+            myCardView.setBackgroundResource(R.drawable.card_background)
+            (myCardView.layoutParams as LinearLayout.LayoutParams).setMargins(30,20,30,0)
+            myCardView.radius = 50f
 
-        val cardView = CardView(this)
-        cardView.radius = 15f
-        cardView.setCardBackgroundColor(Color.parseColor("#009688"))
-        cardView.setContentPadding(50,50,50,50)
-        cardView.setBackgroundResource(R.drawable.card_background)
-        cardView.layoutParams = params
-        cardView.cardElevation = 30f
+
+        val myCardLinearLayout1 = LinearLayout(this)
+            myCardLinearLayout1.layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+            myCardLinearLayout1.orientation = LinearLayout.VERTICAL
+//
+
+        val myCardLinearLayout2 = LinearLayout(this)
+            myCardLinearLayout2.layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+//            (myCardLinearLayout2.layoutParams as LinearLayout.LayoutParams).setMargins(30,20,30,0)
+            myCardLinearLayout2.orientation = LinearLayout.HORIZONTAL
+
+        val myCardLinearLayout3 = LinearLayout(this)
+            myCardLinearLayout3.layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+            myCardLinearLayout3.orientation = LinearLayout.VERTICAL
+
+        val myCardRelativeLayout = RelativeLayout(this)
+            myCardRelativeLayout.layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+
+
+        val myCardLinearLayout4 = LinearLayout(this)
+            myCardLinearLayout4.layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+            myCardLinearLayout4.orientation = LinearLayout.HORIZONTAL
+
 
         val quote = TextView(this)
-        quote.text = ""+categoryList[categoryCounter];
-        quote.textSize = 24f
-        quote.setTextColor(Color.WHITE)
-//        quote.setTypeface(Typeface.SANS_SERIF,Typeface.NORMAL)
-        Log.i("Category: ", ""+categoryList[categoryCounter])
+            quote.text = ""+categoryList[categoryCounter];
+            quote.textSize = 24f
+            quote.setTextColor(Color.WHITE)
+            Log.i("Category: ", ""+categoryList[categoryCounter])
 
         val name = TextView(this)
-        name.text = "We have listed several "+categoryList[categoryCounter]
-        name.textSize = 15f
-//        name.setTypeface(Typeface.MONOSPACE, Typeface.ITALIC)
-        name.setTextColor(Color.parseColor("#E0F2F1"))
+            name.text = "We have listed several "+categoryList[categoryCounter]
+            name.textSize = 15f
+            name.setTextColor(Color.parseColor("#E0F2F1"))
+
+        val img = ImageView(this)
+            img.setImageResource(R.drawable.questionmark)
+            img.layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+            img.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 210)
 
         val btn = Button(this)
-        btn.text = ""+categoryList[categoryCounter]
-        btn.textSize = 15f
-        btn.setBackgroundColor(0xFF426BFF.toInt());
-        btn.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.putExtra("CATEGORY_NAME", btn.text as String)
-            startActivity(intent)
-            finish()
+            btn.text = ""+categoryList[categoryCounter]
+            btn.textSize = 12f
+            btn.setTextColor(0xFFFFFFFF.toInt())
+            btn.setBackgroundColor(0xFF6200EE.toInt())
+            btn.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            (btn.layoutParams as LinearLayout.LayoutParams).setMargins(0,30,0,0)
+            btn.setOnClickListener {
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.putExtra("CATEGORY_NAME", btn.text as String)
+                startActivity(intent)
+                finish()
         }
 
-        cardLinearLayout.addView(quote)
-        cardLinearLayout.addView(name)
-        cardLinearLayout.addView(btn)
-        cardView.addView(cardLinearLayout)
-
-
-
-
-        mainLinearLayout.addView(cardView)
+            mainLinearLayout.addView(myCardView)
+            myCardView.addView(myCardLinearLayout1)
+            myCardLinearLayout1.addView(myCardLinearLayout2)
+            myCardLinearLayout2.addView(myCardLinearLayout3)
+            myCardLinearLayout3.addView(quote)
+            myCardLinearLayout3.addView(name)
+            myCardLinearLayout2.addView((myCardRelativeLayout))
+            myCardRelativeLayout.addView(img)
+            myCardLinearLayout1.addView(myCardLinearLayout4)
+            myCardLinearLayout4.addView(btn)
             categoryCounter++
         }
-//        btn_category.setOnClickListener(){
-//            val intent = Intent(this, LoginActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        }
     }
 
     private fun readJSON() {
@@ -111,9 +145,6 @@ class MainActivity : AppCompatActivity() {
                     Log.i("Response", ""+client)
                 }
                 run("http://161.35.55.28:5000/api/questions/")
-
-                //val inputStream: InputStream = assets.open("dummy_questions.json")
-                //json = inputStream.bufferedReader().use { it.readText() }
 
                 var jsonarr = JSONArray(json)
                 var inc: Int = 0
